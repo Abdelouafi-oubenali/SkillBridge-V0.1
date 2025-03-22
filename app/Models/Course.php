69e9d6bd59f2b47c92745168db41a1cabeae170c
abdelouafi <abdelouafioubenali.com>
2025-03-22 23:17:40 +0000

    ajouté culumn vidio done miodele

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,7 +9,7 @@ class Course extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description','content','category_id','users_id'];
+    protected $fillable = ['title', 'description','content','category_id','users_id','video'];
 
     public function category()
     {
